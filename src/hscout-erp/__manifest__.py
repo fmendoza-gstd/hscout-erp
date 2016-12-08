037ee6e907edbf8dce0dc0945b2eb7b3944faa23
Frank Mendoza <desarrollo@globalstd.com>
2016-12-08 12:30:10 -0600

    First commit

---
+++ src/hscout-erp/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'HelpScout',
+    'version': '1.0.0',
+    'category': 'hscout',
+    'sequence': 20,
+    'summary': 'Help Desk',
+    'description': '',
+    'website': '',
+    'depends': [],
+    'data': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
