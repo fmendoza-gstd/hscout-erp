19941c5e87c0638935e2f54a86fb9438258538c7
IVALDIVIA\ivaldivia <ivaldivia@globalstd.com>
2016-12-19 10:07:43 -0600

    Desplegar datos en página Mailbox Details #32

--- src/hscout-erp/models/mailbox.py
+++ src/hscout-erp/models/mailbox.py
@@ -9,7 +9,7 @@ class Mailbox(models.Model):
     hs_is_helpdesk = fields.Boolean('HelpDesk')
     hs_from_name = fields.Selection([('mailbox', 'Mailbox Name'), ('user', 'User name'), ('custom', 'Custom Name')], 'From Name', default='mailbox')
     hs_default_status = fields.Selection([('active', 'Active'), ('pending', 'Pending'), ('closed', 'Closed')], 'Default Status')
-    hs_default_assignee = fields.Selection([('anyone', 'Active'), ('person_unassigned', 'Person Replying (if Unassigned)'), ('person_unassigned', 'Person Replying')], 'Default Assignee')
+    hs_default_assignee = fields.Selection([('anyone', 'Active'), ('person_reply_unassigned', 'Person Replying (if Unassigned)'), ('person_reply', 'Person Replying')], 'Default Assignee')
     hs_email_template = fields.Selection([('plain', 'Plain Template'), ('fancy', 'Fancy Template')], 'Email Template')
     hs_email_signature = fields.Html('Email Signature')
     
